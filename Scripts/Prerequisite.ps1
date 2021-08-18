119e22cb891cd30502c9b48c4cb08348c4af0517
Amit Agrawal <agrawalamit31@gmail.com>
2021-08-18 14:43:37 +0530

    add all feedbacks

--- Scripts/Prerequisite.ps1
+++ Scripts/Prerequisite.ps1
@@ -4,13 +4,13 @@ param(
     [Parameter(Mandatory = $true)]
     [string]$resourceGroupName,
     [Parameter(Mandatory = $true)]
-    [string]$kvName,
+    [string]$keyVaultName,
     [Parameter(Mandatory = $true)]
     [string]$location,
     [Parameter(Mandatory = $true)]
     [string]$certName,
     [Parameter(Mandatory = $true)]
-    [string]$kvSecretName,
+    [string]$keySecretForServicePrincipal,
     [Parameter(Mandatory = $true)]
     [string]$servicePrincipalName,
     [Parameter(Mandatory = $true)]
@@ -50,31 +50,40 @@ try {
   }
   Write-Host "Pre-requisites check has completed." -ForegroundColor Green
   
-  $isKVAvailable  = $(az keyvault list --query "[?name=='$kvName'] | length(@)"  -g $resourceGroupName)
+  $isKVAvailable  = $(az keyvault list --query "[?name=='$keyVaultName'] | length(@)"  -g $resourceGroupName)
   if($isKVAvailable -eq 1)
   {
-    Write-Host "keyvault: $kvName already available ..." -ForegroundColor Green
+    Write-Host "keyvault: $keyVaultName already available ..." -ForegroundColor Green
   }
   else
   {
-    Write-Host "Creating keyvault: $kvName ..." -ForegroundColor Green
-    az keyvault create --name $kvName --resource-group $resourceGroupName --location $location
+    Write-Host "Creating keyvault: $keyVaultName ..." -ForegroundColor Green
+    az keyvault create --name $keyVaultName --resource-group $resourceGroupName --location $location
   }
 
   Write-Host "Creating selfsigned certificate in keyvault ..." -ForegroundColor Green
   az keyvault certificate get-default-policy | Out-File -Encoding utf8 defaultpolicy.json
-  az keyvault certificate create --vault-name $kvName -n $certName  --policy `@defaultpolicy.json
-  az keyvault certificate download --vault-name $kvName -n $certName -f cert.pem
+  az keyvault certificate create --vault-name $keyVaultName -n $certName  --policy `@defaultpolicy.json
+  az keyvault certificate download --vault-name $keyVaultName -n $certName -f cert.pem
 
-  Write-Host "Creating ServicePrincipal with Password based auth..." -ForegroundColor Green
-  $spnResult = az ad sp create-for-rbac -n $servicePrincipalName  --skip-assignment
-  $spnDetail = $spnResult | ConvertFrom-Json
+  Write-Host "Creating App registration..." -ForegroundColor Green
+  az ad app create --display-name $servicePrincipalName
+  $appId = az ad app list --display-name $servicePrincipalName --query [0].appId
+  Write-Host "Creating Service Principal..." -ForegroundColor Green
+  az ad sp create --id $appId
+
   Write-Host "Importing certificate from keyvault to ServicePrincipal ..." -ForegroundColor Green
-  az ad sp credential reset -n $servicePrincipalName   --keyvault $kvName --cert $certName --append
+  az ad sp credential reset -n $servicePrincipalName   --keyvault $keyVaultName --cert $certName --append
    
-  Write-Host "Adding ServicePrincipal client secret to keyvault: $kvName ..." -ForegroundColor Green
-  az keyvault secret set --vault-name $kvName --name $kvSecretName --value $spnDetail.password
+  Write-Host "Adding ServicePrincipal client secret to keyvault: $keyVaultName ..." -ForegroundColor Green
+  $spnResult = az ad sp credential reset --name $servicePrincipalName
+  $spnDetail = $spnResult | ConvertFrom-Json 
+  az keyvault secret set --vault-name $keyVaultName --name $keySecretForServicePrincipal --value $spnDetail.password
   $spId = az ad sp list --display-name $servicePrincipalName --query [0].appId
+
+  Write-Host "Adding ServicePrincipal to keyvault access policies ..." -ForegroundColor Green
+  $objectId = az ad app show  --id $spId  --query 'objectId'
+  az keyvault set-policy --name $keyVaultName --object-id $objectId  --secret-permissions backup delete get list recover restore set --key-permissions backup create delete get import list recover restore sign update verify   --certificate-permissions  backup create delete deleteissuers get getissuers import list listissuers managecontacts manageissuers recover restore setissuers update
 
   Write-Host "Creating ServiceConnection..." -ForegroundColor Green
   az devops service-endpoint azurerm create --azure-rm-service-principal-id  $spId  --azure-rm-subscription-id  $subscriptionId  --azure-rm-subscription-name  $subscriptionName   --azure-rm-tenant-id  $tenantId  --name $serviceConnectionName  --detect true  --azure-rm-service-principal-certificate-path  'cert.pem'  --org $organizationName  -p $projectName
