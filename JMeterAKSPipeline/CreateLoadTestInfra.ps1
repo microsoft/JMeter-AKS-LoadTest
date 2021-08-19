054f38a8dbb335699b7289c022d8d0d0b3d465a0
Shuvajyoti <shuvajyoti.deb@gmail.com>
2021-08-20 00:36:05 +0530

    Using camelcase for script parameters

--- JMeterAKSPipeline/CreateLoadTestInfra.ps1
+++ JMeterAKSPipeline/CreateLoadTestInfra.ps1
@@ -1,20 +1,20 @@
 param(
     [Parameter(Mandatory = $True)]
-    [string]$Tenant,
+    [string]$tenant,
     [Parameter(Mandatory = $True)]
-    [string]$DefaultNamespace,
+    [string]$defaultNamespace,
     [Parameter(Mandatory = $True)]
-    [string]$ResourceGroup,
+    [string]$resourceGroup,
     [Parameter(Mandatory = $True)]
-    [string]$AksClusterName,
+    [string]$aksClusterName,
     [Parameter(Mandatory = $True)]
-    [string]$SPNClientId,
+    [string]$spnClientId,
     [Parameter(Mandatory = $True)]
-    [string]$SPNClientSecret,
+    [string]$spnClientSecret,
     [Parameter(Mandatory = $True)]
-    [string]$AksRegion,
+    [string]$aksRegion,
     [Parameter(Mandatory = $False)]
-    [string]$NodeVmSize = "Standard_D2s_v3"
+    [string]$nodeVmSize = "Standard_D2s_v3"
 )
 
 function log([string] $message, [string] $color) {
@@ -24,18 +24,24 @@ function log([string] $message, [string] $color) {
 
 #################### Creating AKS Cluster ################################################
 
-log "############# Installing Extension : aks-preview ###############" "DarkYellow"
-az extension add --name aks-preview
-
-az login --service-principal --username $SPNClientId --password $SPNClientSecret --tenant $Tenant
-
-log "############# AKS Cluster Name : $($AksClusterName) ###############" "DarkMagenta"
-log "############# Creating AKS Cluster ###############" "DarkYellow"
-az aks create --resource-group $ResourceGroup --name $AksClusterName --node-vm-size $NodeVmSize --location $AksRegion --service-principal $SPNClientId --client-secret $SPNClientSecret --node-count 3  --min-count 1 --max-count 50 --enable-cluster-autoscaler --enable-aad --enable-azure-rbac --generate-ssh-keys
-log "############# AKS Cluster creation completed ###############" "DarkGreen"
-
-log "############# Getting cluster credentials #############"  "DarkYellow"
-az aks get-credentials --name $AksClusterName --resource-group $ResourceGroup --admin
-
-log "############# Creating AKS Namespace #############" "DarkYellow"
-kubectl create namespace $DefaultNamespace
+try {
+    log "############# Installing Extension : aks-preview ###############" "DarkYellow"
+    az extension add --name aks-preview
+    
+    az login --service-principal --username $spnClientId --password $spnClientSecret --tenant $tenant
+    
+    log "############# AKS Cluster Name : $($aksClusterName) ###############" "DarkMagenta"
+    log "############# Creating AKS Cluster ###############" "DarkYellow"
+    az aks create --resource-group $resourceGroup --name $aksClusterName --node-vm-size $nodeVmSize --location $aksRegion --service-principal $spnClientId --client-secret $spnClientSecret --node-count 3  --min-count 1 --max-count 50 --enable-cluster-autoscaler --enable-aad --enable-azure-rbac --generate-ssh-keys
+    log "############# AKS Cluster creation completed ###############" "DarkGreen"
+    
+    log "############# Getting cluster credentials #############"  "DarkYellow"
+    az aks get-credentials --name $aksClusterName --resource-group $resourceGroup --admin
+    
+    log "############# Creating AKS Namespace #############" "DarkYellow"
+    kubectl create namespace $defaultNamespace
+}
+catch {
+    Write-Error "An error occurred while creating Load Test Infra"
+    Write-Host $_.ScriptStackTrace
+}
